4e2ef32224cb1fe7c249ab2adc03e9e3fc7c13e3
HimalN <himalnimsara888@gmail.com>
2023-11-20 23:20:48 +0530

    add package

--- src/main/java/lk/ijse/shadowStudio/controller/PackagesFormController.java
+++ src/main/java/lk/ijse/shadowStudio/controller/PackagesFormController.java
@@ -1,24 +1,21 @@
 package lk.ijse.shadowStudio.controller;
 
-import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import java.sql.SQLException;
+import java.util.List;
+
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import lk.ijse.shadowStudio.dto.PackageDto;
+import lk.ijse.shadowStudio.dto.tm.CustomerTm;
+import lk.ijse.shadowStudio.dto.tm.PackageTm;
+import lk.ijse.shadowStudio.model.PackagesModel;
 
 public class PackagesFormController{
     @FXML
@@ -37,7 +34,7 @@ public class PackagesFormController{
     private TableColumn<?, ?> colPackageType;
 
     @FXML
-    private ComboBox<?> comboPackageType;
+    private TextField txtPackageType;
 
     @FXML
     private Label lblPackageId;
@@ -55,13 +52,95 @@ public class PackagesFormController{
     private TextField txtPackagePrice;
 
     @FXML
+    private TableView<PackageTm> tblPackages;
+
+    private final PackagesModel packagesModel = new PackagesModel();
+
+    public void initialize(){
+        generateNextPackageId();
+        loadAllPackages();
+        setCellValueFactory();
+
+    }
+    private void clearFields(){
+        lblPackageId.setText("");
+        txtPackageName.setText("");
+        txtAboutPackage.setText("");
+        txtPackagePrice.setText("");
+
+    }
+    private void generateNextPackageId(){
+
+        try {
+            String packageId  = PackagesModel.generateNextPackageId();
+            lblPackageId.setText(packageId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
     void btnDeletePackageOnAction(ActionEvent event) {
 
     }
 
     @FXML
     void btnSavePackageOnAction(ActionEvent event) {
+        String packageId = lblPackageId.getText();
+        String packageName = txtPackageName.getText();
+        String packageType = txtPackageType.getText();
+        String packageAbout = txtAboutPackage.getText();
+        String packagePrice = txtPackagePrice.getText();
 
+        var dto = new PackageDto(packageId,packageName,packageType,packageAbout,packagePrice);
+
+        try {
+            boolean isSaved = PackagesModel.savePackage(dto);
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"Package Saved").show();
+                generateNextPackageId();
+                loadAllPackages();
+                setCellValueFactory();
+
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Error while Saving data").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+    }
+
+    private void setCellValueFactory() {
+        colPackageId.setCellValueFactory(new PropertyValueFactory<>("package_id"));
+        colPackageName.setCellValueFactory(new PropertyValueFactory<>("package_name"));
+        colPackageType.setCellValueFactory(new PropertyValueFactory<>("package_type"));
+        colPackageDescription.setCellValueFactory(new PropertyValueFactory<>("package_description"));
+        colPackagePrice.setCellValueFactory(new PropertyValueFactory<>("package_price"));
+    }
+    private void loadAllPackages() {
+        var model = new PackagesModel();
+
+        ObservableList<PackageTm> obList = FXCollections.observableArrayList();
+        try {
+            List<PackageDto> dtoList = model.getAllPackages();
+
+            for (PackageDto dto : dtoList) {
+                obList.add(
+                        new PackageTm(
+                                dto.getPackage_id(),
+                                dto.getPackage_name(),
+                                dto.getPackage_type(),
+                                dto.getPackage_description(),
+                                dto.getPackage_price()
+                        )
+                );
+            }
+            tblPackages.setItems(obList);
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     @FXML
--- src/main/java/lk/ijse/shadowStudio/model/PackagesModel.java
+++ src/main/java/lk/ijse/shadowStudio/model/PackagesModel.java
@@ -1,4 +1,77 @@
 package lk.ijse.shadowStudio.model;
 
+import lk.ijse.shadowStudio.db.DbConnection;
+import lk.ijse.shadowStudio.dto.CustomerDto;
+import lk.ijse.shadowStudio.dto.PackageDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PackagesModel {
+    public static String generateNextPackageId() throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "SELECT package_id FROM packages ORDER BY package_id LIMIT 1";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        ResultSet resultSet = pstm.executeQuery();
+        if (resultSet.next()){
+            return splitPackageId(resultSet.getString(1));
+
+        }
+
+        return splitPackageId(null);
+    }
+    private static String splitPackageId(String currentPackageId){
+        if(currentPackageId != null) {
+            String[] split = currentPackageId.split("P0");
+
+            int id = Integer.parseInt(split[1]); //01
+            id++;
+            return "P00" + id;
+        } else {
+            return "P001";
+        }
+    }
+
+    public static boolean savePackage(PackageDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+
+        String sql = "INSERT INTO packages VALUES(?,?,?,?,?)";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setString(1, dto.getPackage_id());
+        pstm.setString(2,dto.getPackage_name());
+        pstm.setString(3,dto.getPackage_type());
+        pstm.setString(4,dto.getPackage_description());
+        pstm.setString(5,dto.getPackage_price());
+
+        boolean isSaved = pstm.executeUpdate() > 0;
+
+        return isSaved;
+    }
+    public List<PackageDto> getAllPackages() throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "select * from packages";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        ResultSet rs = pstm.executeQuery();
+
+        ArrayList<PackageDto> dtoList = new ArrayList<>();
+        while (rs.next()) {
+            dtoList.add(
+                    new PackageDto(
+                            rs.getString(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4),
+                            rs.getString(5)
+                    )
+            );
+        }
+        return dtoList;
+
+    }
 }
